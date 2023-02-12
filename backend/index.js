d3619a1af26c364cb8533a3cef37bb0cae76b714
sanjeevani dhavale <sanjeevanidhavale55@gmail.com>
2023-02-13 00:14:53 +0530

    Mern Project

---
+++ backend/index.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const cors = require("cors");
+require('./db/config');
+const User = require("./db/User");
+const Product = require('./db/Product')
+
+const Jwt = require('jsonwebtoken')
+const jwtKey = 'e-com'
+
+const app = express();
+//mongoose.set('strictQuery', true);
+//mongoose.set('strictQuery', false);
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+            resp.send({ result: "Somthing went wrong ,pleace try After a time" })
+        }
+        resp.send({ result, auth: token });
+    })
+})
+
+
+//login
+app.post("/login", async (req, resp) => {
+    console.log(req.body);
+    if (req.body.email && req.body.password) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+                if (err) {
+                    resp.send({ result: "Somthing went wrong ,pleace try After a time" })
+                }
+                resp.send({ user, auth: token });
+            })
+
+        } else {
+            resp.send({ result: 'user not found' })
+        }
+    } else {
+        resp.send({ result: 'user not found' })
+    }
+})
+//add Product
+app.post("/add-product", verifyToken, async (req, resp) => {
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result);
+})
+//list api
+app.get("/products", verifyToken, async (req, resp) => {
+    let products = await Product.find({}).populate("category");
+    if (products.length > 0) {
+        resp.send(products);
+    } else {
+        resp.send({ result: "No Products found" })
+    }
+})
+
+app.delete("/product/:id", verifyToken, async (req, resp) => {
+    //resp.send(req.params.id);
+    const result = await Product.deleteOne({ _id: req.params.id })
+    resp.send(result);
+})
+
+//update
+
+app.get("/product/:id", verifyToken, async (req, resp) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    if (result) {
+        resp.send(result);
+    } else {
+        resp.send({ result: 'user not found' })
+    }
+})
+
+app.put("/product/:id", verifyToken,async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    );
+    resp.send(result);
+})
+
+app.get('/search/:key', verifyToken, async (req, resp) => {
+    let result = await Product.find({
+        "$or": [
+            { name: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } }
+        ]
+    })
+    resp.send(result);
+});
+
+//middleware
+function verifyToken(req, resp, next) {
+    let token = req.headers['authorization'];
+    if (token) {
+        token = token.split(' ')[1];
+        console.log("Middleware called", token);
+        Jwt.verify(token, jwtKey, (err, valid) => {
+            if (err) {
+                resp.status(401).send({ result: 'Please proviad valid token' })
+            } else {
+                next();
+            }
+        })
+    } else {
+        resp.status(403).send({ result: 'Please add token headers' })
+    }
+}
+
+
+app.listen(5000);
---
+++ javascript/connection.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+
+// mongoose.set('strictQuery', false);
+
+// const connectDB = async () => {
+//     mongoose.connect('mongodb://127.0.0.1:27017/e-commerce');
+//     const productSchema = new mongoose.Schema({});
+//     const product = mongoose.model('users', productSchema);
+//     const data = await product.find();
+//     console.warn(data);
+// }
+// connectDB();
+// app.listen(5000);
